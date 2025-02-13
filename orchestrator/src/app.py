249b7789f6849ce96733a6b6606e5dbc45448225
naiaraAM <nam312@alumnos.unican.es>
2025-02-13 20:14:53 +0200

    orchestrator app.py template for services

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from concurrent import futures
+from http.client import responses
 
 # This set of lines are needed to import the gRPC stubs.
 # The path of the stubs is relative to the current file, or absolute inside the container.
@@ -9,6 +11,11 @@ fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/p
 sys.path.insert(0, fraud_detection_grpc_path)
 import fraud_detection_pb2 as fraud_detection
 import fraud_detection_pb2_grpc as fraud_detection_grpc
+
+transaction_verification_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/transaction_verification'))
+sys.path.insert(0, transaction_verification_grpc_path)
+import transaction_verification_pb2 as transaction_verification
+import transaction_verification_pb2_grpc as transaction_verification_grpc
 
 import grpc
 
@@ -20,6 +27,15 @@ def greet(name='you'):
         # Call the service through the stub object.
         response = stub.SayHello(fraud_detection.HelloRequest(name=name))
     return response.greeting
+
+def check_transaction(request):
+    request_json = json.dumps(request)
+    # Establish connection with the transaction_verification gRPC servive.
+    with grpc.insecure_channel('transaction_verification:50052') as channel:
+        stub = transaction_verification_grpc.TransactionVerificationServiceStub(channel)
+        # Call the service through the stub object
+        response = stub.checkTransaction(transaction_verification.TransactionVerificationRequest(json=request_json))
+    return response.code
 
 # Import Flask.
 # Flask is a web framework for Python.
@@ -53,19 +69,47 @@ def checkout():
     # Get request object data to json
     request_data = json.loads(request.data)
     # Print request object data
-    print("Request Data:", request_data.get('items'))
+    print("Request Data:", request_data.get('items')) # on terminal
 
-    # Dummy response following the provided YAML specification for the bookstore
-    order_status_response = {
+    response = None
+
+    # TODO: create microservices
+    with futures.ThreadPoolExecutor() as executor:
+        # Check fraud
+        future_fraud_detection = executor.submit(check_fraud, request_data)
+        # Check transaction
+        future_transaction_verification = executor.submit(check_transaction, request_data)
+        # Get suggestions
+        future_suggestions = executor.submit(get_suggestions, request_data)
+
+        fraud_detection_code = future_fraud_detection.result()
+        transaction_verification_code = future_transaction_verification.result()
+        suggestions = future_suggestions.result()
+    transaction_verification_code = check_transaction(request_data)
+    if fraud_detection_code != "200":
+        # set error message
+        error_response = {
+            'error': {
+                'code': '400',
+                'message': 'Fraud detected'
+            }
+        }
+        return error_response, 400
+    elif transaction_verification_code != "200":
+        # set error message
+        response = {
+            'error': {
+                'code': '400',
+                'message': 'Transaction is invalid'
+            }
+        }
+        return response, 400
+    response = {
         'orderId': '12345',
         'status': 'Order Approved',
-        'suggestedBooks': [
-            {'bookId': '123', 'title': 'The Best Book', 'author': 'Author 1'},
-            {'bookId': '456', 'title': 'The Second Best Book', 'author': 'Author 2'}
-        ]
+        'suggestedBooks': suggestions
     }
-
-    return order_status_response
+    return response
 
 
 if __name__ == '__main__':
