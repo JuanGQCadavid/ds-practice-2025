3dd83c04af63eb9fcf6e5ad9aae22e15bb27204f
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2025-02-17 10:21:24 +0100

    Fix on trans verefication

--- transaction_verification/src/app.py
+++ transaction_verification/src/app.py
@@ -1,26 +1,19 @@
 from datetime import datetime
+from concurrent import futures
+import grpc
 import json
 import sys
 import os
 
-# This set of lines are needed to import the gRPC stubs.
-# The path of the stubs is relative to the current file, or absolute inside the container.
-# Change these lines only if strictly needed.
 FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
 transaction_verification_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/transaction_verification'))
 sys.path.insert(0, transaction_verification_grpc_path)
-import transaction_verification_pb2 as transaction_verification
+
 import transaction_verification_pb2_grpc as transaction_verification_grpc
+import transaction_verification_pb2 as transaction_verification
 
-import grpc
-from concurrent import futures
-
-# Create a class to define the server functions, derived from
-# fraud_detection_pb2_grpc.HelloServiceServicer
 class TransactionVerificationService(transaction_verification_grpc.TransactionVerificationServiceServicer):
-    # Create an RPC function to say hello
     def checkTransaction(self, request, context):
-        # request is a stringified json, obtain the json object
         request_data = json.loads(request.json)
         # Check if the list of items is not empty
         if not request_data.get('items'):
@@ -30,12 +23,12 @@ class TransactionVerificationService(transaction_verification_grpc.TransactionVe
             return response
 
         # Check if required user data is filled in
-        required_fields = ['user', 'creditCard', 'items', 'billingAddress', 'shippingMethod', 'giftWrapping', 'termsAccepted']
+        required_fields = ['user', 'creditCard', 'items', 'billingAddress', 'shippingMethod', 'giftWrapping', 'termsAndConditionsAccepted']
         for field in required_fields:
             if field not in request_data or not request_data[field]:
                 response = transaction_verification.TransactionVerificationResponse()
                 response.code = "400"
-                print("Required field not filled in")
+                print(f"Required {field} field not filled in")
                 return response
 
         # If all checks pass, return a successful response
