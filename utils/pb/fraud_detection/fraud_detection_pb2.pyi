f5e9809e83773d6893d6308cbe612f24c0e03258
naiaraAM <nam312@alumnos.unican.es>
2025-02-18 13:12:59 +0200

    Modified fraud_detection system

--- utils/pb/fraud_detection/fraud_detection_pb2.pyi
+++ utils/pb/fraud_detection/fraud_detection_pb2.pyi
@@ -1,14 +1,24 @@
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
+class CreditCard(_message.Message):
+    __slots__ = ("number", "cvv", "expirationDate")
+    NUMBER_FIELD_NUMBER: _ClassVar[int]
+    CVV_FIELD_NUMBER: _ClassVar[int]
+    EXPIRATIONDATE_FIELD_NUMBER: _ClassVar[int]
+    number: str
+    cvv: str
+    expirationDate: str
+    def __init__(self, number: _Optional[str] = ..., cvv: _Optional[str] = ..., expirationDate: _Optional[str] = ...) -> None: ...
+
 class FraudDetectionRequest(_message.Message):
-    __slots__ = ("json",)
-    JSON_FIELD_NUMBER: _ClassVar[int]
-    json: str
-    def __init__(self, json: _Optional[str] = ...) -> None: ...
+    __slots__ = ("creditCard",)
+    CREDITCARD_FIELD_NUMBER: _ClassVar[int]
+    creditCard: CreditCard
+    def __init__(self, creditCard: _Optional[_Union[CreditCard, _Mapping]] = ...) -> None: ...
 
 class FraudDetectionResponse(_message.Message):
     __slots__ = ("code",)
